2c8fe9eb3e62b7514a3824e2a5c1c3fef3eafde0
Alex Yang <Alex Yang@AlexYang-PC>
2013-11-25 21:58:38 -0800

    Lecture 3 finished. Today I spent most of the time setting up photoshop

---
+++ Java2DGame/src/game/Game.java
@@ -0,0 +1,137 @@
+package game;
+
+import gfx.Screen;
+import gfx.SpriteSheet;
+
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable{
+
+	private static final long serialVersionUID = 1L;
+
+	public static final int WIDTH = 160;
+	public static final int HEIGHT = WIDTH/ 12*9;
+	public static final int SCALE = 5;
+	public static final String NAME = "Game";
+
+	private JFrame frame;
+
+	public boolean running = false;
+	public int tickCount = 0;
+
+	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+	private Screen screen;
+	
+	public Game(){
+		setMinimumSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		setMaximumSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+
+		frame = new JFrame(NAME);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+
+		frame.add(this, BorderLayout.CENTER);
+		frame.pack();
+
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	public void init(){
+		screen = new Screen(WIDTH,HEIGHT, new SpriteSheet("/sprite_sheet.png"));
+	}
+	
+	public synchronized void start(){
+		running = true;
+		new Thread(this).start();
+	}
+
+	public synchronized void stop(){
+		running = false;
+	}
+
+	@Override
+	public void run() {
+		long lastTime = System.nanoTime();
+		double nsPerTick = 1000000000D /60D;
+
+		int frames = 0;
+		int ticks = 0;
+		long lastTimer = System.currentTimeMillis();
+		double delta = 0;
+
+		init();
+		
+		while(running){
+			long now = System.nanoTime();
+			delta += (now - lastTime)/ nsPerTick;
+			lastTime = now;
+			boolean shouldRender = true;
+
+			try {
+				Thread.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			while(delta >= 1){
+				ticks++;
+				tick();
+				delta -= 1;
+				shouldRender = true;
+			}
+
+			if(shouldRender){
+				frames++;
+				render();	//render	
+			}
+
+			if(System.currentTimeMillis() - lastTimer >= 1000){
+				lastTimer += 1000;
+				frames = 0;
+				ticks = 0;
+			}
+		}
+	}
+
+	public void tick(){
+		tickCount++;
+		
+//		screen.xOffset++;
+//		screen.yOffset++;
+	}
+
+	public void render(){
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null){
+			createBufferStrategy(3);//triple buffering
+			return;
+		}
+		
+		screen.render(pixels, 0, WIDTH);
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		
+		g.dispose();
+		bs.show();
+	}
+
+	public static void main(String[] args){
+		new Game().start()	;
+	}
+}
