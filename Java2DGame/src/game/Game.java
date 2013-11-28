1deb4170a6d03441870d2019532e2d6d432c6e12
Alex Yang <Alex Yang@AlexYang-PC>
2013-11-27 20:31:13 -0800

    Finished up to lecture 5. Kinda confused about the pixel manipulation

--- Java2DGame/src/game/Game.java
+++ Java2DGame/src/game/Game.java
@@ -1,5 +1,6 @@
 package game;
 
+import gfx.Colors;
 import gfx.Screen;
 import gfx.SpriteSheet;
 
@@ -12,6 +13,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Game extends Canvas implements Runnable{
 
@@ -21,17 +23,32 @@ public class Game extends Canvas implements Runnable{
 	public static final int HEIGHT = WIDTH/ 12*9;
 	public static final int SCALE = 5;
 	public static final String NAME = "Game";
+	public static final Dimension DIMENSIONS = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
+	public static Game game;
 
 	private JFrame frame;
+
+	private Thread thread;
 
 	public boolean running = false;
 	public int tickCount = 0;
 
 	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+	private int[] colors = new int[6*6*6];
 
 	private Screen screen;
-	
+	public InputHandler input;
+	//	public WindowHandler windowHandler;
+	//	public Level leve;
+	//	public Player player;
+	//	
+	//	public GameClient socketClient;
+	//	public GameServer socketServer;
+
+	public boolean debug = true;
+	public boolean isApplet = false;
+
 	public Game(){
 		setMinimumSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
 		setMaximumSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
@@ -51,16 +68,46 @@ public class Game extends Canvas implements Runnable{
 	}
 
 	public void init(){
+		int index = 0;
+		for(int r = 0; r < 6; r++){
+			for(int g = 0; g < 6; g++){
+				for(int b = 0; b < 6; b++){
+					int rr = (r * 255 / 5);
+					int gg = (r * 255 / 5);
+					int bb = (r * 255 / 5);
+
+					colors[index ++] = rr << 16 | gg << 8 | bb; //Lecture 05 - 5:00
+				}
+			}
+		}
+
 		screen = new Screen(WIDTH,HEIGHT, new SpriteSheet("/sprite_sheet.png"));
+		input = new InputHandler(this);
+		//		level = new Level("/levels/water_test_level.png");
+		//		player = new PlayerMP(level, 100, 100, input, JOptionPane.showInputDialog(this, "Please enter a username"), null, -1);
+		//		level.addEntity(player);
+		//		if(!isApplet){
+		//			Packet))Login loginPacket = new
+		//		}
 	}
-	
+
 	public synchronized void start(){
 		running = true;
-		new Thread(this).start();
+		thread = new Thread(this, NAME + "_main");
+		thread.start();
+		if(!isApplet){
+
+		}
 	}
 
 	public synchronized void stop(){
 		running = false;
+
+		try{
+			thread.join();
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -74,24 +121,24 @@ public class Game extends Canvas implements Runnable{
 		double delta = 0;
 
 		init();
-		
+
 		while(running){
 			long now = System.nanoTime();
 			delta += (now - lastTime)/ nsPerTick;
 			lastTime = now;
 			boolean shouldRender = true;
 
-			try {
-				Thread.sleep(2);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-
 			while(delta >= 1){
 				ticks++;
 				tick();
 				delta -= 1;
 				shouldRender = true;
+			}
+
+			try {
+				Thread.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 
 			if(shouldRender){
@@ -109,9 +156,11 @@ public class Game extends Canvas implements Runnable{
 
 	public void tick(){
 		tickCount++;
-		
-//		screen.xOffset++;
-//		screen.yOffset++;
+
+		if(input.up.isPressed()) screen.yOffset--;
+		if(input.down.isPressed()) screen.yOffset++;
+		if(input.left.isPressed()) screen.xOffset--;
+		if(input.right.isPressed()) screen.xOffset++;
 	}
 
 	public void render(){
@@ -120,13 +169,22 @@ public class Game extends Canvas implements Runnable{
 			createBufferStrategy(3);//triple buffering
 			return;
 		}
-		
-		screen.render(pixels, 0, WIDTH);
-		
+//
+//		for(int y = 0 ; y < 32 ; y++){
+//			for(int x = 0 ; x< 32 ; x++){
+//				screen.render(x << 3, y << 3, 0, Colors.get(555, 500, 050, 005));
+//			}
+//		}
+
+		for(int y = 0 ; y < screen.height ; y++){
+			for(int x = 0 ; x< screen.width ; x++){
+				int colorCode = screen.pixels[x+y * screen.width];
+				if( colorCode < 255) pixels[x+y * WIDTH] = colors[colorCode];
+			}
+		}
+
 		Graphics g = bs.getDrawGraphics();
-		
 		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
-		
 		g.dispose();
 		bs.show();
 	}
--- Java2DGame/src/gfx/Screen.java
+++ Java2DGame/src/gfx/Screen.java
@@ -2,59 +2,71 @@ package gfx;
 
 public class Screen {
 
-	public static final int MAP_WIDTH = 64;
-	public static final int MAP_WIDTH_MASK = MAP_WIDTH -1;
-	
-	public int[] tiles = new int[MAP_WIDTH * MAP_WIDTH];
-	public int[] colors = new int[MAP_WIDTH * MAP_WIDTH * 4]; // we use 4 colors in this game so this contains all the color info
-	
-	public int xOffset = 0;
-	public int yOffset = 0;
-	
-	public int width;
-	public int height;
-	
-	public SpriteSheet sheet;
-	
-	public Screen(int width, int height, SpriteSheet sheet){
-		this.width = width;
-		this.height = height;
-		this.sheet = sheet;
-		
-		for(int i = 0; i< MAP_WIDTH * MAP_WIDTH; i++){
-			colors[i*4 +0]  = 0xff00ff;
-			colors[i*4 +1]  = 0x00ffff;
-			colors[i*4 +2]  = 0xffff00;
-			colors[i*4 +3]  = 0xffffff;
-			
-		}
-	}
-	
-	public void render(int[] pixels, int offset, int row){
-		for(int yTile = yOffset >>3; yTile <= (yOffset+height) >>3; yTile++){
-			int yMin = yTile * 8 - yOffset;
-			int yMax = yMin + 8;
-			if(yMin < 0) yMin = 0;
-			if(yMax > height) yMax = height;
-			
-			for(int xTile = xOffset >>3; xTile <= (xOffset+width) >>3 ; xTile++){
-				int xMin = xTile * 8 - xOffset;
-				int xMax = xMin + 8;
-				if(xMin < 0) xMin = 0;
-				if(xMax > width) xMax = width;
-				
-				int tileIndex = (xTile & (MAP_WIDTH_MASK)) + (yTile &(MAP_WIDTH_MASK)) * MAP_WIDTH;
-				
-				for(int y = yMin; y< yMax; y++){
-					int sheetPixel = ((y+yOffset) & 7) * sheet.width + ((xMin+xOffset) & 7);
-					int tilePixel = offset+xMin + y* row;
-					for(int x = xMin; x < xMax; x++){
-						int color = tileIndex *4 + sheet.pixels[sheetPixel++];
-						pixels[tilePixel++] = colors[color];
-					}
-				}
-			}
-		}
-	}
-}
+    public static final int MAP_WIDTH = 64;
+    public static final int MAP_WIDTH_MASK = MAP_WIDTH - 1;
 
+    public static final byte BIT_MIRROR_X = 0x01;
+    public static final byte BIT_MIRROR_Y = 0x02;
+
+    public int[] pixels;
+
+    public int xOffset = 0;
+    public int yOffset = 0;
+
+    public int width;
+    public int height;
+
+    public SpriteSheet sheet;
+
+    public Screen(int width, int height, SpriteSheet sheet) {
+        this.width = width;
+        this.height = height;
+        this.sheet = sheet;
+
+        pixels = new int[width * height];
+    }
+
+    public void render(int xPos, int yPos, int tile, int colour, int mirrorDir, int scale) {
+        xPos -= xOffset;
+        yPos -= yOffset;
+
+        boolean mirrorX = (mirrorDir & BIT_MIRROR_X) > 0;
+        boolean mirrorY = (mirrorDir & BIT_MIRROR_Y) > 0;
+
+        int scaleMap = scale - 1;
+        int xTile = tile % 32;
+        int yTile = tile / 32;
+        int tileOffset = (xTile << 3) + (yTile << 3) * sheet.width;
+        for (int y = 0; y < 8; y++) {
+            int ySheet = y;
+            if (mirrorY)
+                ySheet = 7 - y;
+
+            int yPixel = y + yPos + (y * scaleMap) - ((scaleMap << 3) / 2);
+
+            for (int x = 0; x < 8; x++) {
+                int xSheet = x;
+                if (mirrorX)
+                    xSheet = 7 - x;
+                int xPixel = x + xPos + (x * scaleMap) - ((scaleMap << 3) / 2);
+                int col = (colour >> (sheet.pixels[xSheet + ySheet * sheet.width + tileOffset] * 8)) & 255;
+                if (col < 255) {
+                    for (int yScale = 0; yScale < scale; yScale++) {
+                        if (yPixel + yScale < 0 || yPixel + yScale >= height)
+                            continue;
+                        for (int xScale = 0; xScale < scale; xScale++) {
+                            if (xPixel + xScale < 0 || xPixel + xScale >= width)
+                                continue;
+                            pixels[(xPixel + xScale) + (yPixel + yScale) * width] = col;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void setOffset(int xOffset, int yOffset) {
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+    }
+}
